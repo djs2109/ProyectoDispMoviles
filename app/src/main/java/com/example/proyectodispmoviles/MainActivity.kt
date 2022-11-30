15d01368b687dc627040c20899de6bf1548a7294
djimenez80640 <djimenez80640@ufide.ac.cr>
2022-11-29 21:37:16 -0600

    Cambio de logo

--- app/src/main/java/com/example/proyectodispmoviles/MainActivity.kt
+++ app/src/main/java/com/example/proyectodispmoviles/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.proyectodispmoviles
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import com.example.proyectodispmoviles.databinding.ActivityMainBinding
 import com.google.firebase.FirebaseApp
@@ -10,6 +11,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+
 
 
 
@@ -35,6 +37,8 @@ class MainActivity : AppCompatActivity() {
 
         //Click Login
         binding.btLogin.setOnClickListener{login()}
+
+
 
     }
 
